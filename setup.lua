6a08d89ae1ce33b5616b17f43c3fdcd59b34511d
Zekkos <dullcraft@gmail.com>
2026-06-23 15:04:56 +1200

    Really need to learn wget

--- setup.lua
+++ setup.lua
@@ -1,6 +1,5 @@
-local basalt_dir = "basalt"
-local basalt_cmd = "wget run https://raw.githubusercontent.com/Pyroxenium/Basalt2/main/install.lua -f "
-local pid_cmd = "wget https://raw.githubusercontent.com/Zekkos/CC_Zeklib/main/api/pid.lua"
+local basalt_cmd = "wget run https://raw.githubusercontent.com/Pyroxenium/Basalt2/main/install.lua -f basalt"
+local pid_cmd = "wget https://raw.githubusercontent.com/Zekkos/CC_Zeklib/main/api/pid.lua pid"
 
 shell.run(basalt_cmd)
 shell.run(pid_cmd)
