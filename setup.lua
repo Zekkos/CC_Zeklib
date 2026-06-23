0296574832c7e6cc55fee8c23b70e2f8a965f089
Zekkos <dullcraft@gmail.com>
2026-06-23 15:10:32 +1200

    AAAHHHH

--- setup.lua
+++ setup.lua
@@ -1,5 +1,5 @@
 local basalt_cmd = "wget run https://raw.githubusercontent.com/Pyroxenium/Basalt2/main/install.lua -f api/basalt"
-local pid_cmd = "wget -r https://raw.githubusercontent.com/Zekkos/CC_Zeklib/main/api/"
+local pid_cmd = "wget https://raw.githubusercontent.com/Zekkos/CC_Zeklib/main/api/pid.lua"
 
 shell.run(basalt_cmd)
 shell.run("cd api")
