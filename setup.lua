f7bfdd1409817b60383c5259a5bfe04179f2f626
Zekkos <dullcraft@gmail.com>
2026-06-23 15:08:02 +1200

    This is getting stupid

--- setup.lua
+++ setup.lua
@@ -1,5 +1,6 @@
-local basalt_cmd = "wget run https://raw.githubusercontent.com/Pyroxenium/Basalt2/main/install.lua -f basalt"
-local pid_cmd = "wget https://raw.githubusercontent.com/Zekkos/CC_Zeklib/main/api/pid.lua pid"
+local basalt_cmd = "wget run https://raw.githubusercontent.com/Pyroxenium/Basalt2/main/install.lua -f api/basalt"
+local pid_cmd = "wget -r https://raw.githubusercontent.com/Zekkos/CC_Zeklib/main/api/"
 
 shell.run(basalt_cmd)
+shell.run("cd api")
 shell.run(pid_cmd)
