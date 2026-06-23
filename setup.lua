a19f907949cb442c6583d866063ec04a49670df1
Zekkos <dullcraft@gmail.com>
2026-06-23 14:59:20 +1200

    Hopefully this fixed it

--- setup.lua
+++ setup.lua
@@ -1,6 +1,6 @@
 local basalt_dir = "api/basalt"
 local basalt_cmd = "wget run https://raw.githubusercontent.com/Pyroxenium/Basalt2/main/install.lua -f " .. basalt_dir
-local pid_cmd = "wget https://raw.githubusercontent.com/Zekkos/CC_Zeklib/refs/heads/main/api/pid.lua -f api/pid.lua"
+local pid_cmd = "wget https://raw.githubusercontent.com/Zekkos/CC_Zeklib/main/api/pid.lua -f api/pid.lua"
 
 shell.run(basalt_cmd)
 shell.run(pid_cmd)
