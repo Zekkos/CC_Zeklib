ae64ecec5697da0f3ddc0a24b03a9971265c3437
Zekkos <dullcraft@gmail.com>
2026-06-23 15:02:28 +1200

    Removed the attempt at having multiple paths, not really needed for simple library

--- setup.lua
+++ setup.lua
@@ -1,6 +1,6 @@
-local basalt_dir = "api/basalt"
-local basalt_cmd = "wget run https://raw.githubusercontent.com/Pyroxenium/Basalt2/main/install.lua -f " .. basalt_dir
-local pid_cmd = "wget https://raw.githubusercontent.com/Zekkos/CC_Zeklib/main/api/pid.lua -f api/pid.lua"
+local basalt_dir = "basalt"
+local basalt_cmd = "wget run https://raw.githubusercontent.com/Pyroxenium/Basalt2/main/install.lua -f "
+local pid_cmd = "wget https://raw.githubusercontent.com/Zekkos/CC_Zeklib/main/api/pid.lua"
 
 shell.run(basalt_cmd)
 shell.run(pid_cmd)
